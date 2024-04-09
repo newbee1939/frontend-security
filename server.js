f25a7a2fddd5e4ac5630266da29d1386d9f34b0b
newbee1939 <hideto3970@gmail.com>
2024-04-10 07:22:25 +0900

    public追加

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// ミドルウェアの登録
+app.use(express.static("public"));
+
 app.get("/", (req, res, next) => {
     res.end("Top Page");
 });
